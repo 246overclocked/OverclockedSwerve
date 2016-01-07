8e7186ea2a097f8380c68c2a2edd676f5837b04f
PaulTerrasi <paul.a.terrasi@gmail.com>
2016-01-07 01:30:53 -0500

    Add example Drivetrain object to Robot

--- src/org/usfirst/frc/team246/robot/Robot.java
+++ src/org/usfirst/frc/team246/robot/Robot.java
@@ -1,7 +1,12 @@
 
 package org.usfirst.frc.team246.robot;
 
+import org.usfirst.frc.team246.nav6.IMUAdvanced;
+
+import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.Timer;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -11,12 +16,45 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  * directory.
  */
 public class Robot extends IterativeRobot {
+	
+	public Drivetrain drivetrain;
+	
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {
-
+    	PIDConstants speedPIDConstants = new PIDConstants(1, 0, 0, .5, 0);
+    	PIDConstants anglePIDConstants = new PIDConstants(1, 0, 0, 0, 0);
+    	SwerveModule[] swerves = {
+        	new SwerveModule(new CANTalon(0, 1), new CANTalonPotentiometer(0, 5, 360, 720), 14, 360, 0, 12, speedPIDConstants, anglePIDConstants, 1, 0, 0, "frontModule"),
+        	new SwerveModule(new CANTalon(0, 2), new CANTalonPotentiometer(0, 6, 360, 720), 14, 360, 0, -12, speedPIDConstants, anglePIDConstants, 1, 0, 0, "rearModule"),
+        	new SwerveModule(new CANTalon(0, 3), new CANTalonPotentiometer(0, 7, 360, 720), 14, 360, 12, 0, speedPIDConstants, anglePIDConstants, 1, 0, 0, "rightModule"),
+        	new SwerveModule(new CANTalon(0, 4), new CANTalonPotentiometer(0, 8, 360, 720), 14, 360, -12, 0, speedPIDConstants, anglePIDConstants, 1, 0, 0, "leftModule"),
+        };
+    	
+    	//We were having occasional errors with the creation of the nav6 object, so we make 5 attempts before allowing the error to go through and being forced to redeploy.
+        IMUAdvanced navX;
+    	int count = 0;
+        int maxTries = 5;
+        while(true) {
+            try {
+                navX = new IMUAdvanced(new SerialPort(57600,SerialPort.Port.kMXP), (byte)50);
+                if(navX != null) break;
+            } catch (Exception e) {
+                if (++count == maxTries)
+                {
+                    e.printStackTrace();
+                    break;
+                }
+                Timer.delay(.01);
+            }
+        }
+        
+        PIDConstants crabPIDConstants = new PIDConstants(1, 0, 0, 0, 20);
+    	PIDConstants twistPIDConstants = new PIDConstants(1, 0, 0, 0, 20);
+        
+        drivetrain = new Drivetrain(swerves, navX, crabPIDConstants, twistPIDConstants);
     }
 
     /**
